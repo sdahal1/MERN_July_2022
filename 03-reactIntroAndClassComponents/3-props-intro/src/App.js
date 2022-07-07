8c2d7a38ebb0d35cae62fa20853939447c551107
sdahal1 <raabdahl@gmail.com>
2022-07-07 14:03:24 -0700

    pm lecture class components complete

--- 03-reactIntroAndClassComponents/3-props-intro/src/App.js
+++ 03-reactIntroAndClassComponents/3-props-intro/src/App.js
@@ -6,11 +6,21 @@ function App() {
   return (
     <div className="App">
       <h1>Dojo Diner Menu</h1>
-      <MenuItem dishName = {"Basil Calamari- Thai Style"} price={12.00} description={"Fried squid tho"}></MenuItem>
+      <MenuItem dishName = {"Basil Calamari- Thai Style"} price={12.00}>
+        <p>Its fried Squid.</p>
+        <em>Note: This food contains the following allergens: Seafood, Gluten</em>
+      </MenuItem>
 
-      <MenuItem dishName = {"Strogonoff"} price={15.00} description={"Pasta tho"}></MenuItem>
+      <MenuItem dishName = {"Strogonoff"} price={15.00}>
+        <p>Its Pasta with beef meat.</p>
+        <em>Note: This food contains the following allergens: Gluten</em>
+      </MenuItem>
 
-      <MenuItem dishName = {"Buffalo Wings"} price={8.00} description={"Comes with one wing tho"}></MenuItem>
+      <MenuItem dishName = {"Buffalo Wings"} price={8.00}>
+        <p>Its only one wing.</p>
+        <strong>Please select spice level between 1-5</strong>
+        <em>Note: This food contains the following allergens: Seafood, Gluten</em>
+      </MenuItem>
 
     </div>
   );
--- 03-reactIntroAndClassComponents/3-props-intro/src/components/MenuItem.jsx
+++ 03-reactIntroAndClassComponents/3-props-intro/src/components/MenuItem.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 
 class MenuItem extends Component{
+    constructor(props) {
+        super(props);
+        //state is a special type of variable that we can change the value of and when the value of a state variable changes, the component reloads on the screen with the newly changed information
+        this.state = {
+            price: this.props.price //transferring my props variable into my state variable so that change the state variable (increment the price)
+        };
+    }
     render(){
         return(
             <div className="item">
                 <h3>Name: {this.props.dishName}</h3>
-                <p>Price: ${this.props.price}</p>
-                <p>Description: {this.props.description}</p>
+                <p>Price: ${this.state.price}</p>
+                {this.props.children}
+                <button className='btn btn-success' onClick={()=>this.setState({price: this.state.price+1})}>Click me for inflation for {this.props.dishName}</button>
             </div>
         )
     }
 }
+
+//when we click on a button something happens
+//the thing that happens is the price of the menu item goes up by one
 
 export default MenuItem;
